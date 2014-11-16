c2851dacb4ca6a765df6070160a2ced7b662e045
Sasi <sasi@gatech.edu>
2014-11-16 15:54:16 -0500

    One ball rotates around parent
    Also started adding other method headers and local vars needed for
    multiple child rotation

--- Assets/RotateAroundBehavior.cs
+++ Assets/RotateAroundBehavior.cs
@@ -3,16 +3,38 @@ using System.Collections;
 
 public class RotateAroundBehavior : MonoBehaviour {
 
-	private int numOrbiters;
+	public int numOrbiters;
 	public GameObject sourceObject;
 	public GameObject[] orbiters;
+	private int orbitRadius = 3;
 
 	// Use this for initialization
 	void Start () {
+		Init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		for(int i = 0; i < numOrbiters; i++){
+			UpdateOrbiterPosition(i);
+		}
+	}
+
+	void Init(){
+		//set initial positions of balls
+		//1 ball can be anywhere
+		//2 balls should be opposite
+
+		SetOrbiterPositions();
+		Vector3 orbitPos = new Vector3(transform.position.x + orbitRadius, transform.position.y, transform.position.z);
+		orbiters[0].GetComponent<Transform>().localPosition = orbitPos;
+	}
+
+	void SetOrbiterPositions(){
+		float angleDiv = 360f / (float)numOrbiters;
+	}
+
+	void UpdateOrbiterPosition(int orbiter){
+		orbiters[0].GetComponent<Transform>().RotateAround(sourceObject.transform.position, Vector3.forward, 100 * Time.deltaTime);
 	}
 }
