1ac3f8b7a88f0f877508f3f3491bda146ebe4a7f
Sasi <sasi@gatech.edu>
2014-11-16 16:01:08 -0500

    Rotation, yay!
    Now rotates given a number of orbiters between 0 and 5

--- Assets/RotateAroundBehavior.cs
+++ Assets/RotateAroundBehavior.cs
@@ -26,15 +26,19 @@ public class RotateAroundBehavior : MonoBehaviour {
 		//2 balls should be opposite
 
 		SetOrbiterPositions();
-		Vector3 orbitPos = new Vector3(transform.position.x + orbitRadius, transform.position.y, transform.position.z);
-		orbiters[0].GetComponent<Transform>().localPosition = orbitPos;
 	}
 
 	void SetOrbiterPositions(){
 		float angleDiv = 360f / (float)numOrbiters;
+
+		Vector3 orbitPos = new Vector3(transform.position.x + orbitRadius, transform.position.y, transform.position.z);
+		for(int i = 0; i < numOrbiters; i++){
+			orbiters[i].GetComponent<Transform>().localPosition = orbitPos;	//move to initial pos
+			orbiters[i].GetComponent<Transform>().RotateAround(sourceObject.transform.position, Vector3.forward, angleDiv * i);//rotate
+		}
 	}
 
 	void UpdateOrbiterPosition(int orbiter){
-		orbiters[0].GetComponent<Transform>().RotateAround(sourceObject.transform.position, Vector3.forward, 100 * Time.deltaTime);
+		orbiters[orbiter].GetComponent<Transform>().RotateAround(sourceObject.transform.position, Vector3.forward, 100 * Time.deltaTime);
 	}
 }
