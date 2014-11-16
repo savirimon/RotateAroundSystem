dae33b230a8e766f2308a22789dbecab4a287756
Sasi <sasi@gatech.edu>
2014-11-16 15:17:56 -0500

    Initial commit

---
+++ Assets/RotateAroundBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundBehavior : MonoBehaviour {
+
+	private int numOrbiters;
+	public GameObject sourceObject;
+	public GameObject[] orbiters;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
